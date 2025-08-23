7ade0cde09393577fb619b1b9f5eeb47fbca6737
TiagoCoder2022 <tiagonewalves@hotmail.com>
2025-08-22 22:50:36 -0300

    feat: add NotFoundException

---
+++ api/src/main/java/com/contactslist/api/shared/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.contactslist.api.shared.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
