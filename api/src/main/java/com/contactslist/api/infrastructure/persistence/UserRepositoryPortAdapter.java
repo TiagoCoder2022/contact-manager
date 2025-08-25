11e586f1d63ca4bb5698a9e90b53b7cd3eca8e85
TiagoCoder2022 <tiagonewalves@hotmail.com>
2025-08-24 21:15:48 -0300

    refactor: rename variables in UserRepositoryPostAdapter

---
+++ api/src/main/java/com/contactslist/api/infrastructure/persistence/UserRepositoryPortAdapter.java
@@ -0,0 +1,24 @@
+package com.contactslist.api.infrastructure.persistence;
+
+import com.contactslist.api.domain.model.User;
+import com.contactslist.api.domain.repository.UserRepositoryPort;
+import com.contactslist.api.infrastructure.persistence.jpa.UserJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class UserRepositoryPortAdapter implements UserRepositoryPort {
+    private final UserJpaRepository repo;
+
+    public Optional<User> findByEmailOrUsername(String emailOrUsername){
+        return repo.findByEmailOrUsername(emailOrUsername, emailOrUsername);
+    }
+    public boolean existsByEmail(String email){ return repo.existsByEmail(email); }
+    public boolean existsByUsername(String userName){ return repo.existsByUsername(userName); }
+    public User save(User user){ return repo.save(user); }
+    public Optional<User> findById(UUID id){ return repo.findById(id); }
+}
